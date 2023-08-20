cf4a117bb0b88899ad1de61f35aa9fbdb6514a85
PaniNastia <pomazuevanasta@gmail.com>
2023-08-20 17:12:04 +0200

    strona w budowie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./gfx/logo.png";
+import logoP from "./gfx/logo_p.png";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Seite in Bearbeitung</p>
+    <div className="layout">
+      <header>
+        <div className="container">
+          <img src={logo} alt="okoheim" />
+        </div>
       </header>
+      <main>
+        <div className="container height">
+          <div>
+            <img src={logoP} alt="okoheim" className="img-fluid" />
+            <h1 class="display-4 mt-2">Seite in Bearbeitung</h1>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
